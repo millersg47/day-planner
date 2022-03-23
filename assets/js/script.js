8d4207b146704521428b28d8e45d3dc416a3c290
Sophie Miller <millersg47@gmail.com>
2022-03-23 12:03:30 -0700

    updates for loops to check specific indexes instead of whole arrays, adds data-attribute ids to hour divs, makes localStorage render onto correct elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,63 @@
-var saveButton = document.queryCommandIndeterm(".saveBtn");
-//element that will change depending on current time
+//element for save buttons, used with event listener to store content in corresponding text area
+var saveButton = document.querySelectorAll(".saveBtn");
+//element that will change background color depending on current time and holds the text that will be stored when save button is clicked
 var hourBlockEl = document.querySelectorAll(".hour-content");
+//element that holds the hour text
+var hourEl = document.querySelectorAll(".hour");
 //shows current date in header
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY")); 
 
+//loads values from local storage and renders it in the corresponding hourBlockEl
 function renderEntries() {
-  localStorage.getItem("entry");
+  for ( var i = 0; i < hourEl.length; i++) { 
+    var time = hourEl[i].textContent;
+    var text = localStorage.getItem(time);
+    hourEl[i].nextElementSibling.value = text;
+  }
 }
+renderEntries();
 
-
-//element that holds the hour text to be compared to current time for color coding
-var hourEl = document.querySelectorAll(".hour");
+//array to hold the integer of the id for each hourEl. Used later to compare against the current time to dictate color coding
 var hourArray = [];
-// for loop to collect hour text into array for comparing against current time check
+// for loop to collect the id data attribute value into hourArray for comparing against current time 
 for ( var i = 0; i < hourEl.length; i++) {
   console.log(hourEl[i]);
-  var hourVal = hourEl[i].textContent;
+  var hourVal = parseInt(hourEl[i].getAttribute("id"));
   hourArray.push(hourVal);
 }
 console.log(hourArray);
 
-var timeNow = moment();
-timeNow = timeNow.format("h a"); 
-var timeNowStr = JSON.stringify(timeNow);
-timeNowStr =timeNowStr.toUpperCase();
-console.log(timeNowStr);
-timeNow = timeNow.toUpperCase();
+//current time to compare against each time block
 var hourNow = moment();
-hourNow = hourNow.format("h");
+//parse the moment object into an integer
+hourNow = parseInt(hourNow.format("H"));
+console.log(hourNow);
 
-//for loop to check if hourArray value equals timeNow and update class accordingly. Not working, need to fix. Also, unsure how to check for past or future hours. 
+//for loop to check if the value of hourArray at each index equals hourNow var, then update class accordingly.
 for ( var i = 0; i < hourArray.length; i++) {
-  if (timeNowStr == hourArray[i]) {
-    hourBlockEl.classList.add("present");
+  console.log(hourArray[i]);
+  if (hourNow == hourArray[i]) {
+    hourBlockEl[i].classList.add("present");
+  } else if (hourNow < hourArray[i]) {
+    hourBlockEl[i].classList.add("future");
+  } else {
+    hourBlockEl[i].classList.add("past");
   }
 }
 
-
-// var entry = hourBlockEl.textContent;
-// console.log(entry);
-// //event listener on save button to store entry text. 
-// saveButton.addEventListener("click", function () {
-//   if(entry == null) {
-//     return;
-//   }
-//   localStorage.setItem("entry", entry);
-//   renderEntries();
-// });
+//event listener on save button to store entry text of corresponding hourBlockEl. 
+for ( var i = 0; i < saveButton.length; i++) { 
+  saveButton[i].addEventListener("click", function (event) {
+    var entry = event.target.previousElementSibling.value;
+    console.log(entry);
+    if(entry == null) {
+      return;
+    }
+    var timeName = event.target.previousElementSibling.previousElementSibling.textContent;
+    localStorage.setItem(timeName, entry);
+  })
+};
 
 
 
